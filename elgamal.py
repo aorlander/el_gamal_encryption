046477e5ba3d9313199825138ec16a98d953e7fa
aorlander <49505752+aorlander@users.noreply.github.com>
2021-01-27 17:46:37 -0500

    implement keygen, encrypt, decrypt from instructions

--- elgamal.py
+++ elgamal.py
@@ -5,10 +5,8 @@ from params import p
 from params import g
 
 # returns secret key [1..p] and public key g^a mod p
-# The modular inverse of A mod C is the B value that makes A * B mod C = 1
 def keygen():
-    # q is the order of g
-    q = mod_inverse(g, 1)
+    q = mod_inverse(g, p)    # q is the order of g
     a = random.SystemRandom().randint(1, q)
     h = pow(g,a,p)
     sk = a
@@ -17,13 +15,15 @@ def keygen():
 
 # take public key, h, and integer, m, and return an El Gamal ciphertext
 def encrypt(pk,m):
-    c1 = 0
-    c2 = 0
+    # to encrypt m, generate r in the range 1 <= r <= q
+    r = random.SystemRandom().randint(1, q)
+    c1 = pow(g,r,p)
+    c2 = pow(pow(h,r) * m,1,p)
     return [c1,c2]
 
 # take private key, a, and ciphertext [c1,c2] and return an integer m
 def decrypt(sk,c):
-    m = 0
+    m = (c,1,p)
     return m
 
 # The python interpreter actually executes the function body here
